400d9ecfc288433518ca137fb418c5165c2e8c91
Your Name <m.karaarslan06@outlook.com>
2024-03-12 23:55:13 +0300

    personel controller updated

--- src/controllers/personnel.controller.js
+++ src/controllers/personnel.controller.js
@@ -15,24 +15,28 @@ module.exports = {
       data,
     });
   },
+
   create: async (req, res) => {
-    //isLead Controller
+    // isLead Control:
     const isLead = req.body?.isLead || false;
     if (isLead) {
-      await Personnel.updateMany(
+      const xyz = await Personnel.updateMany(
         { departmentId: req.body.departmentId, isLead: true },
         { isLead: false }
       );
     }
 
     const data = await Personnel.create(req.body);
+
     res.status(201).send({
       error: false,
       data,
     });
   },
+
   read: async (req, res) => {
     const data = await Personnel.findOne({ _id: req.params.id });
+
     res.status(200).send({
       error: false,
       data,
@@ -40,6 +44,7 @@ module.exports = {
   },
 
   update: async (req, res) => {
+    // isLead Control:
     const isLead = req.body?.isLead || false;
     if (isLead) {
       const { departmentId } = await Personnel.findOne(
@@ -52,7 +57,10 @@ module.exports = {
       );
     }
 
-    const data = await Personnel.updateOne({ _id: req.params.id }, req.body);
+    const data = await Personnel.updateOne({ _id: req.params.id }, req.body, {
+      runValidators: true,
+    });
+
     res.status(202).send({
       error: false,
       data,
@@ -63,14 +71,13 @@ module.exports = {
   delete: async (req, res) => {
     const data = await Personnel.deleteOne({ _id: req.params.id });
 
-    const isDeleted = data.deletedCount >= 1 ? true : false;
-
-    res.status(isDeleted ? 204 : 404).send({
-      error: isDeleted,
+    res.status(data.deletedCount ? 204 : 404).send({
+      error: !data.deletedCount,
       data,
     });
   },
-  // LOGIN & LOGOUT
+
+  // LOGIN & LOGOUT:
 
   login: async (req, res) => {
     const { username, password } = req.body;
